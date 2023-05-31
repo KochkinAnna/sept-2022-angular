ffb8e2bdc6d5cfcf8d2b3860f897a8315a722bb1
KochkinAnna <kochkinaanichka@gmail.com>
2023-05-31 13:32:00 +0300

    lesson 4 part 1 resolver

--- src/app/modules/postModule/components/post-details/post-details.component.ts
+++ src/app/modules/postModule/components/post-details/post-details.component.ts
@@ -11,13 +11,11 @@ import {IPost} from "../../../../interfaces";
 export class PostDetailsComponent implements OnInit {
   post: IPost;
 
-  constructor(private activatedRoute: ActivatedRoute, private postService: PostService) {
+  constructor(private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({id}) => {
-      this.postService.getById(id).subscribe(value => this.post = value)
-    })
+    this.activatedRoute.data.subscribe(({postData}) =>this.post=postData)
   }
 
 }
